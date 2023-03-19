e5583596b10582da3c8e8ce5a32272a7c735e017
mishaalkandapath <mishaalhere@gmail.com>
2023-03-19 15:34:29 -0400

    storing bricks in an array

--- breakout.asm
+++ breakout.asm
@@ -21,7 +21,7 @@ ADDR_DSPL:
 ADDR_KBRD:
     .word 0xffff0000
 #store all the brick pixels
-# bricks: .space 
+bricks: .space 1280
 
 ##############################################################################
 # Mutable Data
@@ -37,12 +37,8 @@ ADDR_KBRD:
 
 main:
     # Initialize the game
-    #setting up the basic brick colors
-    li $t1 , 0xea2014 #$t1 = red
-    li $t2 , 0x4F6F23 #$t2 = green
-    li $t3 , 0xFBB533 #$t3 = orange
-    li $t9 , 0x000000 #black
     lw $t4, ADDR_DSPL #load in the address to the display
+    la $t2, bricks #array of bricks
     
     li $t5, 0 # the first position to start writing in pixels
     
@@ -50,35 +46,51 @@ brick_loop: bge $t5, 1280, game_loop #while 0 <
     addu $t7, $t4, $t5 #offset display address by t5 and store the offset in t7
     li $t8, 20 # every third pixel drawn needs to be empty 
     divu $t5, $t8 # division for the same
-    mfhi $t0
+    mfhi $t0 #get the value in remainder
+    #draw the respective colors with alternating black rows
     blt $t5, 256, brick_reds
     blt $t5, 512, fill_black
     blt $t5, 768, brick_oranges
     blt $t5, 1024, fill_black
     blt $t5, 1280, brick_greens
-    
+
+##FILLING IN ROWS ###
 brick_reds:
+    li $t1 , 0xea2014 #$t1 = red
     beq $t0, 16, fill_black
     sw $t1, 0($t7) # set pixel in t7 to red
+    sw $t1, 0($t2) #load the pixel value into the brick array
     addi $t5, $t5, 4
+    addi $t2, $t2, 4 #offset the array address
     j brick_loop
     
 brick_oranges:
+    li $t1 , 0xFBB533 #$t3 = orange
     beq $t0, 8, fill_black
-    sw $t3, 0($t7) # set pixel in t7 to red
+    sw $t1, 0($t7) # set pixel in t7 to red
+    sw $t1, 0($t2) #load the pixel value into the brick array
     addi $t5, $t5, 4
+    addi $t2, $t2, 4 #offset the array address
     j brick_loop
 
 brick_greens:
+    li $t1 , 0x4F6F23 #$t1 = green
     beq $t0, 0, fill_black
-    sw $t2, 0($t7) # set pixel in t7 to red
+    sw $t1, 0($t7) # set pixel in t7 to red
+    sw $t1, 0($t2) #load the pixel value into the brick array
     addi $t5, $t5, 4
+    addi $t2, $t2, 4 #offset the array address
     j brick_loop
     
 fill_black:
-    sw $t8, 0($t7)
+    li $t1, 0x000000 #black
+    sw $t1, 0($t7)
+    sw $t1, 0($t2) #load the pixel value into the brick array
     addi $t5, $t5, 4
+    addi $t2, $t2, 4 #offset the array address
     j brick_loop
+    
+##FILLING IN ROWS ENDS###
 
 game_loop:
 	# 1a. Check if key has been pressed
