17b8f7d6b52c71034cf2f02093f7ec5b08897a91
mishaalkandapath <mishaalhere@gmail.com>
2023-03-22 00:19:59 -0400

    moving balls and keyboard management

--- breakout_fix.asm
+++ breakout_fix.asm
@@ -188,37 +188,77 @@ interaction_setup:
  wait_for_keyboard_to_begin:
     #take keyboard input to begin the game
     lw $t8, 0($t0)                  # Load first word from keyboard
-    beq $t8, 1, move_ball_init     # If first word 1, key is pressed
+    beq $t8, 1, game_loop     # If first word 1, key is pressed
     j wait_for_keyboard_to_begin    
+    
+game_loop:
+	# 1a. Check if key has been pressed
+    # 1b. Check which key has been pressed
+    # 2a. Check for collisions
+	# 2b. Update locations (paddle, ball)
+	# 3. Draw the screen
+	# 4. Sleep
+    
+# go through all the bricks in the array
+# if the current direction is (0, 1) then check if ball is one row below any brick
+#if the current direction is (1, 0), then check if the ball is one col to the left of any brick
+#if the current direction is (-1, 0), then check if the ball is one col to the right of any brick 
+#if the current direction is (0, -1), then check if the ball is one row above any brick
+    #the next pixel value of this given current direction:
+    li $t7, -4
+    mult $s4, $t7
+    mflo $t7
+    add $t7, $t7, $s3
+    
+    li $t6, -256
+    mult $s5, $t6
+    mflo $t6
+    add $t6, $t6, $t7 #t6 stores the new value of pixel
+    
+    collision_check_above: beq $s5, 1, check_brick_above #check if the ball is below a brick
+    collision_check_below: beq $s5, -1, check_brick_below #check if the ball is above a brick
+    collision_check_right: beq $s4, 1, check_brick_right #ball is left of any brick
+    collision_check_left:  beq $s4, -1, check_brick_left #ball is right of any brick
+    collision_check_paddle: beq $s5, -1, check_collision_paddle #check if the ball is going to collide with the paddle
 
-move_ball_init:
+#if we get here then there are no collisions, we can safely move the ball in specified direction:
+move_ball:
+    li $t7, -4
+    mult $s4, $t7
+    mflo $t7
+    add $t7, $t7, $s3
+    
+    li $t6, -256
+    mult $s5, $t6
+    mflo $t6
+    add $t6, $t6, $t7 #t6 stores the new value of pixel
+    
     #update the position of the ball:
     li $t1, 0x000000 #black
-    addu $t6, $t4, $s3 #get the offset to the display to get to the ball
-    sw $t1, 0($t6) #fill out the current position of the ball
-    subiu $s3, $s3, 256 #move it a row up
-    addu $t6, $t4, $s3 #get new ball position
+    addu $t7, $t4, $s3 #get the offset to the display to get to the ball
+    sw $t1, 0($t7) #fill out the current position of the ball
     li $t1, 0xffffff #white
+    addu $s3, $t6, $zero
+    addu $t6, $t4, $t6 #get the offset to the display to get to the ball
     sw $t1, 0($t6)
     
- movement_keyboard:
-    #take keyboard input to begin the game
+
+#can also check for keyboard inputs to move the paddle:
+movement_keyboard:
+    #take keyboard input to move the paddle
     lw $t8, 0($t0)                  # Load first word from keyboard
     beq $t8, 1, receive_keyboard_in     # If first word 1, key is pressed
-    j movement_keyboard     
+    j loop_again  
     
 receive_keyboard_in:
     lw $a0, 4($t0)                  # Load second word from keyboard
-    
-    li $v0, 1                       # ask system to print $a0
-    syscall
-    
+   
     beq $a0, 0x61, move_paddle_left     # Check if left was pressed
     beq $a0, 0x64, move_paddle_right #similarly for right
     
-    j receive_keyboard_in
+    j loop_again
     
- move_paddle_left:
+move_paddle_left:
      beq $s0, 30976, movement_keyboard # cant move left no more
      #move the paddle sections left by one
      
@@ -243,7 +283,7 @@ receive_keyboard_in:
      addu $t7, $s0, $t4 #get the offset to the display to get to the left paddle
      sw $t1, 0($t7) #fill out the left paddle
      
-     j movement_keyboard
+     j loop_again
      
  
  move_paddle_right:
@@ -271,16 +311,133 @@ receive_keyboard_in:
      addu $t7, $s0, $t4 #get the offset to the display to get to the left paddle
      sw $t1, 0($t7) #fill out the left paddle
      
-     j movement_keyboard
-     
+     j loop_again
     
-game_loop:
-	# 1a. Check if key has been pressed
-    # 1b. Check which key has been pressed
-    # 2a. Check for collisions
-	# 2b. Update locations (paddle, ball)
-	# 3. Draw the screen
-	# 4. Sleep
+check_brick_above:
+    #in this case the y direction is 1:
+    #$t2 stores the address of the array
+    addiu $t7, $t2, 3083 #the final index in the array 
+    blt $t6, $t7, may_collide_above
+    
+    j collision_check_below
+ 
+may_collide_above:
+     #the index of where it could collide is in t7
+     addu $t7, $t2, $t6 #offset the array memory address
+     lw $t8, 0($t7) #load in the value stored
+     bne $t8, 0x000000, collides_brick_above
+     j collision_check_below
 
-    #5. Go back to 1
-    # b game_loop
+collides_brick_above:
+    li $t1, 0x000000 
+    sw $t1, 0($t7)
+    neg $s5, $s5
+    j movement_keyboard
+ 
+check_brick_below:
+    #in this case the y direction is -1:
+    #$t2 stores the address of the array
+    addiu $t7, $t2, 3083 #the final index in the array 
+    blt $t6, $t7, may_collide_below
+    
+    j collision_check_right
+    
+may_collide_below:
+    #the index of where it could collide is in t7
+    addu $t7, $t2, $t6 #offset the array memory address
+    lw $t8, 0($t7) #load in the value stored
+    bne $t8, 0x000000, collides_brick_below
+    j collision_check_right
+    
+collides_brick_below:
+    li $t1, 0x000000 
+    sw $t1, 0($t7)
+    neg $s5, $s5
+    j movement_keyboard
+
+check_brick_left:
+    #in this case the x direction is -1:
+    #$t2 stores the address of the array
+    addiu $t7, $t2, 3083 #the final index in the array 
+    blt $t6, $t7, may_collide_left
+    
+    j collision_check_paddle
+    
+may_collide_left:
+    #the index of where it could collide is in t7
+    addu $t7, $t2, $t6 #offset the array memory address
+    lw $t8, 0($t7) #load in the value stored
+    bne $t8, 0x000000, collides_brick_left
+    j collision_check_paddle
+    
+collides_brick_left:
+    li $t1, 0x000000 
+    sw $t1, 0($t7)
+    neg $s4, $s4 
+    j movement_keyboard
+    
+check_brick_right:
+    #in this case the x direction is 1:
+    #$t2 stores the address of the array
+    addiu $t7, $t2, 3083 #the final index in the array 
+    blt $t6, $t7, may_collide_right
+    
+    j collision_check_left
+    
+may_collide_right:
+    #the index of where it could collide is in t7
+    addu $t7, $t2, $t6 #offset the array memory address
+    lw $t8, 0($t7) #load in the value stored
+    bne $t8, 0x000000, collides_brick_right
+    j collision_check_left
+    
+collides_brick_right:
+    li $t1, 0x000000 
+    sw $t1, 0($t7)
+    neg $s4, $s4 
+    j movement_keyboard
+
+check_collision_paddle:
+    #in this case the y direction of the ball is -1 with some x direction
+    #s0 - s2 store offsets in memory of paddle
+    addu $t7, $t4, $s0 #getting the address of the left paddle
+    beq $t7, $t6, may_collide_paddle_left
+    addu $t7, $t4, $s1 #getting the address of the right paddle
+    beq $t7, $t6, may_collide_paddle_right
+    addu $t7, $t4, $s2 #getting the address of the middle paddle
+    beq $t7, $t6, may_collide_paddle_middle
+    
+may_collide_paddle_middle:
+    neg $s4, $s4
+    neg $s5, $s5
+    j receive_keyboard_in
+    
+may_collide_paddle_left:
+    li $s5, 1
+    li $s4, -1
+    j receive_keyboard_in
+    
+may_collide_paddle_right:
+    li $s5, 1
+    li $s4, 1
+    j receive_keyboard_in
+
+
+#5. Go back to 1
+loop_again:    
+    li $a0, 10
+    li $v0, 32
+    syscall
+    j game_loop
+
+move_ball_init:
+    #update the position of the ball:
+    li $t1, 0x000000 #black
+    addu $t6, $t4, $s3 #get the offset to the display to get to the ball
+    sw $t1, 0($t6) #fill out the current position of the ball
+    subiu $s3, $s3, 256 #move it a row up
+    addu $t6, $t4, $s3 #get new ball position
+    li $t1, 0xffffff #white
+    sw $t1, 0($t6)
+    
+    
